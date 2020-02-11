2a3992aeb4e4137c71422844f1b60f27fdcc569f
JamesSwatton <jamessswatton@gmail.com>
2020-02-11 11:31:55 +0000

    add basic movement

--- main.js
+++ main.js
@@ -5,9 +5,13 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+let hasFoundHat = false;
+let hasFallenDownHole = false;
+
 class Field {
     constructor(field) {
         this.field = field;
+        this.pathCurrentPos = [0, 0];
     }
 
     print() {
@@ -18,6 +22,29 @@ class Field {
             )
         })
     }
+
+    isInField() {
+
+    }
+
+    move(direction) {
+        switch (direction) {
+            case 'l':
+                this.pathCurrentPos[1] -= 1;
+                break;
+            case 'r':
+                this.pathCurrentPos[1] += 1;
+                break;
+            case 'u':
+                this.pathCurrentPos[0] -= 1;
+                break;
+            case 'd':
+                this.pathCurrentPos[0] += 1;
+                break;
+        }
+        this.field[this.pathCurrentPos[0]][this.pathCurrentPos[1]] = '*';
+    }
+
 }
 
 const myField = new Field([
@@ -27,3 +54,19 @@ const myField = new Field([
 ]);
 
 myField.print();
+
+function isValidDirection(direction) {
+    const validDirections = ['l', 'r', 'u', 'd'];
+    return validDirections.includes(direction);
+}
+
+do {
+    // game stuff in here
+    const direction = prompt('Which way?');
+    // console.log(direction);
+    if (isValidDirection(direction)) {
+        myField.move(direction);
+        myField.print();
+    }
+} while (!hasFoundHat || !hasFallenDownHole) {
+ }
